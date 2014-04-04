e83835c5ad8aeff61e9c4443994e718ebc9c88dc
Julian Weber <jweber@avarteq.de>
2014-04-04 03:12:35 +0200

    move require statementes into prod env checked area

--- examples/rails_carrierwave_initializer.rb
+++ examples/rails_carrierwave_initializer.rb
@@ -1,8 +1,7 @@
-require 'carrierwave'
-require 'a9s_swift'
-
 BUCKET_NAME = "images"
 
 if Rails.env.production?
+  require 'carrierwave'
+  require 'a9s_swift'
   Anynines::Swift::Utility.configure_carrierwave(BUCKET_NAME)
 end
--- examples/rails_paperclip_initializer.rb
+++ examples/rails_paperclip_initializer.rb
@@ -1,8 +1,7 @@
-require 'paperclip'
-require 'a9s_swift'
-
 BUCKET_NAME = "images"
 
 if Rails.env.production?
+  require 'paperclip'
+  require 'a9s_swift'
   Anynines::Swift::Utility.configure_carrierwave(BUCKET_NAME)
 end
